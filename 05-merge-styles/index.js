4af64548b47051e453a340c6a5407bc682794019
Mikhail Koryakin <sdrr87@gmail.com>
2024-01-22 15:38:31 +0300

    refactor: merge styles task with promises

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,40 +1,32 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
-
-process.on('uncaughtException', (err) => {
-  console.error(`There was an uncaught error: ${err}`);
-  process.exit(1);
-});
+const handleError = require('../01-read-file/index');
 
 const stylesDirectoryPath = path.join(__dirname, 'styles');
-const distPath = path.join(__dirname, 'project-dist');
+const distPath = path.join(__dirname, 'project-dist', 'bundle.css');
 
-function mergeStyles(from, to) {
-  fs.readdir(from, { withFileTypes: true }, (err, files) => {
-    if (err) throw err;
-    else {
-      const styles = [];
-      files.forEach((file) => {
-        const isCss = file.name.split('.')[1] === 'css';
-        if (file.isFile() && isCss) {
-          const filePath = path.join(from, file.name);
-          fs.readFile(filePath, 'utf8', (err, data) => {
-            if (err) throw err;
-            else {
-              styles.push(data);
-              fs.writeFile(
-                path.join(to, 'bundle.css'),
-                styles.join('\n'),
-                (err) => {
-                  if (err) throw err;
-                },
-              );
-            }
-          });
-        }
-      });
+async function mergeStyles(from, to) {
+  try {
+    const files = await fs.readdir(from, { withFileTypes: true });
+    const styles = [];
+    for (const file of files) {
+      const isCss = file.name.split('.')[1] === 'css';
+      if (file.isFile() && isCss) {
+        const filePath = path.join(from, file.name);
+        const data = await fs.readFile(filePath, 'utf8');
+        styles.push(data);
+      }
     }
-  });
+    await fs.writeFile(to, styles.join('\n'));
+    console.log('Styles successfully merged!');
+  } catch (err) {
+    handleError(err);
+  }
 }
 
-mergeStyles(stylesDirectoryPath, distPath);
+// run only if direct execution
+if (require.main === module) {
+  mergeStyles(stylesDirectoryPath, distPath);
+}
+
+module.exports = { mergeStyles };
